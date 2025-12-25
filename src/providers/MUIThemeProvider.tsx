f2fc5c616e642235c2986beda28b6718a1fa5069
mazam5 <mohammedazam651@gmail.com>
2025-12-25 16:10:26 +0530

    added: landing page UI

--- src/providers/MUIThemeProvider.tsx
+++ src/providers/MUIThemeProvider.tsx
@@ -1,29 +1,65 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-const darkTheme = createTheme({
-    palette: {
-        mode: 'light',
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    background: {
+      default: '#f7f8f9',
     },
-    typography: {
-        fontFamily: 'Roboto, sans-serif',
-    },
-    components: {
-        MuiButton: {
-            styleOverrides: {
-                root: {
-                    textTransform: 'none',
-                },
-            },
+  },
+  typography: {
+    fontFamily: 'Rubik, Roboto, sans-serif',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          fontFamily: 'Rubik, Roboto, sans-serif',
+          userSelect: 'none',
         },
+      },
     },
+    MuiButton: {
+      styleOverrides: {
+        fullWidth: {
+          width: '100%',
+        },
+        containedPrimary: {
+          backgroundColor: '#6c25ff',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#6c05ff',
+          },
+        },
+        containedSecondary: {
+          backgroundColor: '#cebafb',
+          color: 'black',
+          '&:hover': {
+            backgroundColor: '#b8a8f8',
+          },
+        },
+        root: {
+          textTransform: 'none',
+          borderRadius: '10px',
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          width: '100%',
+        },
+      },
+    },
+  },
 });
 
 const MUIThemeProvider = ({ children }: { children: React.ReactNode }) => (
-    <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        {children}
-    </ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    {children}
+  </ThemeProvider>
 );
 
 export default MUIThemeProvider;
